dc1cb0645153e983dd931ce155f775754b2a810f
kpill <kamalapillai@berkeley.edu>
2020-09-12 14:51:45 -0700

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,7 @@
-
+var fields={};
+document.addEventListener("DOMContentLoaded", function() {
+ fields.firstName = document.getElementById('firstName');
+ fields.lastName = document.getElementById('lastName');
+ fields.email = document.getElementById('email');
+ fields.question = document.getElementById('message');
+})
